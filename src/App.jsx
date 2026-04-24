57926f8fe60e8b9fb1658b17c8c67e39b68c7c18
JOL25 <jeankarloorozco141@gmail.com>
2026-04-24 13:06:10 -0500

    arreglo para que funcione con home

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Habitaciones from './pages/Habitaciones.jsx';
 import Home from './pages/Home.jsx';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/habitaciones" element={<Habitaciones />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
