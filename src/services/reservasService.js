3779004b3add18296124e798e233f0b0b4183073
JOL25 <jeankarloorozco141@gmail.com>
2026-06-28 01:23:20 -0500

    pago simulado

--- src/services/reservasService.js
+++ src/services/reservasService.js
@@ -3,3 +3,7 @@ import hotelApi from './hotelApi.js';
 export function crearReserva(payload) {
   return hotelApi.post('/reservas', payload);
 }
+
+export function confirmarPagoSimulado(reservaId) {
+  return hotelApi.post(`/reservas/${reservaId}/confirmar-pago-simulado`);
+}
